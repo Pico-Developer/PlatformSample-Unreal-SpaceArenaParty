06e5533e69fb4b29f970d778ae905bff677059a6
liangjiahe <liangjiahe@bytedance.com>
2023-04-13 14:34:55 +0800

    删除 github中的 plugins文件

--- Plugins/OnlineSubsystemPICO/Source/OnlineSubsystemPico/Private/Pico_Leaderboard.cpp
+++
@@ -1,1 +0,0 @@
-﻿#include "Pico_Leaderboard.h"
--- Plugins/PicoSpatialAudio/Source/PicoSpatialAudio/Private/PicoSpatialAudioSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PicoSpatialAudioSettings.h"
-
-UPicoSpatialAudioSettings::UPicoSpatialAudioSettings()
-	: RenderingMode(EPxrAudioSpatializer_RenderingMode::Medium_Quality)
-{
-}
